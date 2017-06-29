cebffba43d88ff5575b524ced2be6dafafd4328d
3traker <3traker@ukr.net>
2017-06-29 19:21:10 +0300

    Add example of mutator for hierarchical name

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    private function getParentNameRecursively($parent_id)
+    {
+        $parent = self::find($parent_id);
+        $parent_name = '';
+
+        if($parent->parent_id > 0){
+            $parent_name .= $this->getParentNameRecursively($parent->parent_id) . ' > ';
+        }
+        return $parent_name . $parent->name;
+    }
+
+    public function getHierarchicalNameAttribute()
+    {
+        $hierarchical_name = '';
+
+        if($this->parent_id > 0){
+            $hierarchical_name .= $this->getParentNameRecursively($this->parent_id) . ' > ' ;
+        }
+        return $hierarchical_name .$this->name;
+    }
 }
